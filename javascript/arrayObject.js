31415ce33121f3f9203519093b56d376253c0404
Rithk <rithkchaudhary86@gmail.com>
2023-12-26 21:35:30 +0530

    kidneyproblem

---
+++ javascript/arrayObject.js
@@ -0,0 +1,16 @@
+const arr = [{
+    name: "john",
+    kidney: [{
+        healthy: false
+    },
+    {
+        healthy: true
+    },
+    {
+        healthy: true
+    }]
+}]
+
+const kidney=arr[0].kidney;
+console.log(kidney);
+console.log(kidney[0]);
