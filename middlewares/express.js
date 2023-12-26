65cf0568b158ac172a250918476188807218089a
Rithk <rithkchaudhary86@gmail.com>
2023-12-26 23:04:18 +0530

    middleware

--- middlewares/express.js
+++ middlewares/express.js
@@ -16,6 +16,8 @@ users = [{
 }];
 
 function userMiddleware(req, res, next) {
+    const username=req.headers.username;
+    const password=req.headers.password;
     if (username != "naruto" || password != "123") {
         res.status(403).json({
             msg: "You are not authorized user",
