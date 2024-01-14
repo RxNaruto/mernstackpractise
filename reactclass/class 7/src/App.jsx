a57a8e4e52d08805ba3162b934671827fb751d9d
Rithk <rithkchaudhary86@gmail.com>
2024-01-14 16:50:19 +0530

    suspense api

--- reactclass/class 7/src/App.jsx
+++ reactclass/class 7/src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter,Routes,Route, useNavigate } from "react-router-dom";
-import { Dashboard } from "./components/Dashboard";
-import { Landing } from "./components/Landing";
+const Dashboard = React.lazy(()=> import('./components/Dashboard'))
+const Landing = React.lazy(()=> import('./components/Landing'))
 
 function App(){
   
@@ -13,8 +13,8 @@ function App(){
     <BrowserRouter>
     <Appbar></Appbar>
     <Routes>
-      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-      <Route path="/" element={<Landing></Landing>}></Route>
+      <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard></Dashboard></Suspense>}></Route>
+      <Route path="/" element={<Suspense fallback={"loading..."}><Landing></Landing></Suspense>}></Route>
 
     </Routes>
     </BrowserRouter>
