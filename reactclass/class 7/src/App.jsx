388244552a84c6393ac7f4dab183e17aee813710
Rithk <rithkchaudhary86@gmail.com>
2024-01-14 15:50:59 +0530

    routing

---
+++ reactclass/class 7/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
+
+function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/dashboard" element={Dashboard}></Route>
+      <Route path="/" element={Landing}></Route>
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
