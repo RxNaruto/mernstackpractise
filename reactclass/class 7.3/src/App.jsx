e396f089b9c801d07ebf7e1963ae764f994846cc
Rithk <rithkchaudhary86@gmail.com>
2024-01-19 22:10:41 +0530

    recoil

---
+++ reactclass/class 7.3/src/App.jsx
@@ -0,0 +1,18 @@
+// const { useState } = require("react")
+
+const App=()=>{
+  // const[count,setCount]=useState(0);
+
+  return (
+    <>
+    <button>Home</button>
+    <button>My Network ()</button>
+    <button>Jobs ()</button>
+    <button>Messaging ()</button>
+    <button>My Network ()</button>
+    <button>Me</button>
+    </>
+  )
+}
+
+export default App
