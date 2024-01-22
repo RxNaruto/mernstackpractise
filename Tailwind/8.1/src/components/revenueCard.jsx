1087ca5eb374761839abb8cad61826caa71936d2
Rithk <rithkchaudhary86@gmail.com>
2024-01-22 21:04:23 +0530

    tailwind

--- Tailwind/8.1/src/components/revenueCard.jsx
+++ Tailwind/8.1/src/components/revenueCard.jsx
@@ -1,6 +1,6 @@
 export const RevenueCard=({title,orderCount,amount})=>{
-    return <div className="bg-white rounded shadow-sm">
-        <div>
+    return <div className="bg-white rounded shadow-sm p-4">
+        <div className="text-gray-700">
         {title}
         ?
         </div>
@@ -10,7 +10,11 @@ export const RevenueCard=({title,orderCount,amount})=>{
             </div>
 
             {orderCount ? <div>
-                {orderCount} order(s) {">"}
+                {orderCount} order(s)
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+</svg>
+
                 </div> : null}
         </div>
         
