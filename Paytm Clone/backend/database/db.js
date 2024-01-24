ccded66720aea9d06ec4fe0fc26056d312a43565
Rithk <rithkchaudhary86@gmail.com>
2024-01-24 21:39:08 +0530

    user DataBase Schema

--- Paytm Clone/backend/database/db.js
+++ Paytm Clone/backend/database/db.js
@@ -1,14 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://naruto:jVSNMMGwfSVPzHXV@cluster0.78poq1d.mongodb.net/paytm");
 
-const userSchema = new Schema({
-    username: String(),
-    password: String(),
-    firstName: String(),
-    lastName: String()
-})
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
 
-const Users = mongoose.Schema('User', userSchema);
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const Users = mongoose.model('User', userSchema);
 
 module.exports={
     Users
