275cdea2896b8c17fa4ce263bbedbbee1c9953e0
Rithk <rithkchaudhary86@gmail.com>
2024-01-25 18:36:33 +0530

    user Route

--- Paytm Clone/backend/routes/user.js
+++ Paytm Clone/backend/routes/user.js
@@ -4,18 +4,22 @@ const router = express.Router;
 const {Users} = require("../database/db");
 const { JWT_SECRET } = require('../config');
 
-const userSchema = zod.object({
+const signupSchema = zod.object({
     username: zod.string().email(),
     password: zod.string(),
     firstName: zod.string(),
     lastName: zod.string()
 
 })
+const signinSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
 
 
-router.post("/signup",async (req,res,next)=>{
+router.post("/signup",async (req,res)=>{
     const body = req.body;
-    const inputValidation = userSchema.safeParse(body);
+    const inputValidation = signupSchema.safeParse(body);
     if(!inputValidation.success){
         return res.status(411).json({
             msg:"Wrong input / user already exist"
@@ -50,6 +54,36 @@ router.post("/signup",async (req,res,next)=>{
         token: token
     })
  
+
+})
+
+router.post("/signin",async (req,res)=>{
+    const body = req.body;
+    const inputValidation = signinSchema.safeParse(body);
+    if(!inputValidation.success){
+        return res.status(411).json({
+            msg: "incorrect input"
+        })
+    }
+    const checkUser = await Users.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if(checkUser){
+        const token = jwt.sign({
+            userId: checkUser._id
+        },JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        msg:"Error while logging in"
+    })
 
 })
 
