f4ce0adf88469703764bd010f9f00a6649387c75
Rithk <rithkchaudhary86@gmail.com>
2024-01-28 12:32:32 +0530

    paytm frontend

---
+++ Paytm Clone/frontend/src/pages/Signin.jsx
@@ -0,0 +1,8 @@
+import {Heading} from "../components/Heading"
+export const Signin=()=>{
+    return <div className="bg-slate-300 h-screen flex ">
+        <div className="flex flex-col justify-center"></div>
+        <div></div>
+
+    </div>
+} 
