796760581b972395eb4029331aba6aa87f03f60e
Rithk <rithkchaudhary86@gmail.com>
2024-02-03 09:54:31 +0530

    route endpoint

---
+++ mini projects/Student register/frontend/src/components/Heading.jsx
@@ -0,0 +1,7 @@
+export const Heading=({label})=>{
+    return <div>
+        <div className="text-2xl font-bold font-mono">
+            {label}
+        </div>
+    </div>
+}
---
+++ mini projects/Student register/frontend/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import { Heading } from "../components/Heading";
+
+export const Signup=()=>{
+    return(
+        <div>
+            <div>
+                <div>
+                    <Heading>{"Sign-up"}</Heading>
+                </div>
+            </div>
+        </div>
+    )
+}
